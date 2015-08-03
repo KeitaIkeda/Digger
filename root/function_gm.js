b2f539ad62ef09afc5901c5d4ed9a41d61e5556a
KeitaIkeda-iMac <ket.chg.theworld@gmail.com>
2015-08-04 00:48:31 +0900

    岩に当たった時に一度は砕けない処理を追加 今まではエアーが30減るだけで1回で通過できたが 1度あたりエアーが30減った後2回目はエアー損失なしで通過できるように変更

--- root/function_gm.js
+++ root/function_gm.js
@@ -114,6 +114,14 @@ function flg_eve() {
         };
     } else if (flg[arw_pos.x][arw_pos.y] == f_n.iwa_p) {
         air -= 30;
+        flg[arw_pos.x][arw_pos.y] = f_n.iwa_s;
+        if (event.keyCode == 40) {
+            arw_pos.y -= 1;
+        } else if (event.keyCode == 39) {
+            arw_pos.x -= 1;
+        } else if (event.keyCode == 37) {
+            arw_pos.x += 1;
+        };
     };
 };
 
